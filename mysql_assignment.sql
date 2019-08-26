33104971d553db88662983ca2e59c17f9288e2ab
Iftekher <mhshs.imamun@gmail.com>
2019-08-26 18:01:07 -0400

    creation of mysql db

---
+++ mysql_assignment.sql
@@ -0,0 +1,16 @@
+create table assignment(
+id int,
+order_id int,	
+customer_id varchar(50),
+created_at varchar(50),
+updated_at varchar(50),
+closed_at varchar(50),
+processed_at int,
+status varchar(20)
+number int,
+total_price int,	
+total_less_tax int,
+total_tax int,
+payment_status varchar(10))
+
+select * from assignment 
