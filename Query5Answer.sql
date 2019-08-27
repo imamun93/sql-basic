f1733e4a53faa692d90880eba2bc57fded94a5a8
Iftekher <mhshs.imamun@gmail.com>
2019-08-27 15:25:07 -0400

    fix minor error

--- Query5Answer.sql
+++ Query5Answer.sql
@@ -1,5 +1,5 @@
 SELECT customer_id as 'Customer ID', MAX(created_at) as 'Last Order Date' ,  
 DATE_ADD(MAX(created_at), INTERVAL  DATEDIFF(MAX(created_at), MIN(created_at)) DAY) as 'Next Projected Expected date',
-(sum(total_price - total_tax)/2) as 'Next Projected Order Value'
+(sum(total_price - total_tax)/COUNT(order_id)) as 'Next Projected Order Value'
 FROM order_details GROUP BY customer_id
 HAVING COUNT(*) > 1
